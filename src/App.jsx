6ef350e967d26df4d111ea5c6d3b500151503cfc
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-05-26 22:46:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+// rafce / rafc
+import "./index.css"; // ili './App.css' zavisno od vašeg fajla
+import React from "react";
+
+export const App = () => {
+  return <div className="text-5xl">App</div>;
+};
