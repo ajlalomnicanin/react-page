f70288f37000d7b92763c9cfc7071ab9d10d4f87
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-06-02 18:04:43 +0200

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // rafce / rafc
+import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
 export const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+export const Hero = () => {
+  return (
+    <div>
+      <section className="bg-indigo-700 py-20 mb-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center ">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+              Become a React Dev
+            </h1>
+            <p className="my-4 text-xl text-white">
+              Find the React job that fits your skills and needs
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+// sm: (640px)
+// md: (768px)
+// lg: (1024px)
+// xl: (1280px)
+// 2xl: (1536px)
+
+// max-w-sm: Postavlja maksimalnu širinu elementa na malu veličinu (24rem ili 384px).
+// max-w-md: Postavlja maksimalnu širinu elementa na srednju veličinu (28rem ili 448px).
+// max-w-lg: Postavlja maksimalnu širinu elementa na veliku veličinu (32rem ili 512px).
+// max-w-xl: Postavlja maksimalnu širinu elementa na veoma veliku veličinu (36rem ili 576px).
+
+// max-w-xs: 20rem (320px)
+// max-w-sm: 24rem (384px)
+// max-w-md: 28rem (448px)
+// max-w-lg: 32rem (512px)
+// max-w-xl: 36rem (576px)
+// max-w-2xl: 42rem (672px)
+// max-w-3xl: 48rem (768px)
+// max-w-4xl: 56rem (896px)
+// max-w-5xl: 64rem (1024px)
+// max-w-6xl: 72rem (1152px)
+// max-w-7xl: 80rem (1280px)
+// max-w-full: 100%
