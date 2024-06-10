fea6c689c950d892702de84b5dacfbce3cabe1d6
ajlalomnicanin <ajlalomnicanin11@gmail.com>
2024-06-10 20:33:52 +0200

    Job Listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // rafce / rafc
 import { Hero } from "./components/Hero";
 import { HomeCards } from "./components/HomeCards";
+import { JobListing } from "./components/JobListing";
 import { Navbar } from "./components/Navbar";
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
+      <JobListing />
     </>
   );
 };
